45123a99237663dd2125591d326dc6c56f2b4b32
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-11-22 05:12:17 +0900

    Revert "Revert "기존 크롤링 코드""
    This reverts commit bc8e9d5879a99e8c6688693220d65faf63ef8928.

---
+++ main.py
@@ -0,0 +1,6 @@
+import py_json
+
+src = "test.json"
+dic = {'title': 'tapered raw denim (red selvage)', 'link': 'www.musinsa.com/app/goods/2128118', 'price': '71,400원'}
+
+py_json.json_write_dict(src, dic)
