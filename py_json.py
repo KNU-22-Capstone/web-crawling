c17d4c8150231c05d5d4edbdafc59c363b056f34
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-11-22 05:12:53 +0900

    json 파일 읽기, 쓰기 기능 추가

---
+++ py_json.py
@@ -0,0 +1,23 @@
+import json
+
+def json_read(src):
+    try:
+        with open(src,'r', encoding='utf-8') as f:
+            json_data = json.load(f)
+            return json_data
+    except:
+        print("파일 읽기 실패")
+        print("주소 :", src)
+
+def json_write_dict(src, dic):
+    try:
+        with open(src,'w', encoding='utf-8') as f:
+            json.dump(dic, f, indent="\t")
+            return True
+    except:
+        print("파일 쓰기 실패")
+        print("주소 :", src)
+        print("dic :", dic)
+
+if __name__ == '__main__':
+    print("python에서 json 파일 읽기")
