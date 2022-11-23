0341e52bd33a26b6aa940485e0d729225a1e865d
BreathIN423 <36834513+BreathIN423@users.noreply.github.com>
2022-11-24 06:32:00 +0900

    firebase_connect DB 수정 기능 추가
    + 테스트 firebase DB 추가

--- firebase_connect.py
+++ firebase_connect.py
@@ -5,18 +5,27 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 # 서비스 계정의 비공개 키 파일이름
-cred = credentials.Certificate("codiwiki-tonton-firebase-key.json")
-firebase_admin.initialize_app(cred, {'projectId':'codiwiki-tonton'})
+# cred = credentials.Certificate("codiwiki-tonton-firebase-key.json")
+# firebase_admin.initialize_app(cred, {'projectId':'codiwiki-tonton'})
+# cred = credentials.Certificate("tttest-95867-firebase-key.json")
+# firebase_admin.initialize_app(cred, {'projectId':'tttest-95867'})
+cred = credentials.Certificate("test1124-a9535-firebase-key.json")
+firebase_admin.initialize_app(cred, {'projectId':'test1124-a9535'})
+
 db = firestore.client()
 
 def firebase_write(collection_name, new_dic_list): # 데이터 쓰기
     try:
         dic_name_list = [i['name'] for i in firebase_read(collection_name)]
 
+        write_count = 0
+
         for dic in new_dic_list:
             if dic['name'] not in dic_name_list:
                 db.collection(u'{}'.format(collection_name)).document().set(dic)
+                write_count += 1
 
+        print('DB에 추가한 의류 딕셔너리 수 :', write_count)
     except:
         print("firebase 데이터 쓰기 실패")
         print("collections_name :", collection_name)
@@ -32,6 +41,28 @@ def firebase_read(collection_name): # 데이터 읽기
     except:
         print("firebase 데이터 읽기 실패")
         print("collections_name :", collection_name)
+
+def firbase_fix(collection_name, new_dic_list):
+    try:
+        fix_count = 0
+        users_ref = db.collection(u'{}'.format(collection_name))
+        docs = users_ref.stream()
+        for doc in docs:
+            name = doc.to_dict()['name']
+            print(f'{name} 수정중...')
+            for new_dic in new_dic_list:
+                if name in new_dic['name']:
+                    print('수정!!')
+                    db.collection(u'{}'.format(collection_name)).document(doc.id).update(new_dic)
+                    fix_count += 1
+                    break
+
+        print('DB에서 수정한 의류 딕셔너리 수 :', fix_count)
+    except:
+        print("firebase 데이터 수정 실패")
+        print("collections_name :", collection_name)
+
+
 
 if __name__ == '__main__':
     print("codiwiki-tonton : firebase 접속 모듈입니다.")
--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import json
 import firebase_connect
 import web_crawling
 
-dic = [{'name': '레이어드 크루 넥 반팔 티셔츠_일반 기장 [화이트]', 'price': 16900, 'detail_tag': 'SHORT_SLEEVE', 'picture_URL': '//image.msscdn.net/images/goods_img/20210825/2086653/2086653_1_125.jpg', 'site_URL': 'www.musinsa.com/app/goods/2086653', 'site_name': 'musinsa', 'major_tag': 'TOP', 'views': 0}, {'name': '레이어드 크루 넥 반팔 티셔츠_긴 기장 [화이트]', 'price': 1690015190, 'detail_tag': 'SHORT_SLEEVE', 'picture_URL': '//image.msscdn.net/images/goods_img/20200402/1382658/1382658_7_125.jpg', 'site_URL': 'www.musinsa.com/app/goods/1382658', 'site_name': 'musinsa', 'major_tag': 'TOP', 'views': 0}]
-firebase_connect.firebase_write('의류',dic)
 firebase_connect.firebase_write('의류',web_crawling.run())
+# firebase_connect.firbase_fix('의류', web_crawling.run())
 
 # for i in web_crawling.run():
 #     print(i)
